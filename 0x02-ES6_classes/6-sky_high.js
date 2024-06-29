574a3e588ec0ca07aa51b582199ea010f51aeb2d
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-06-29 23:22:08 +0100

    Implemented a class named SkyHighBuilding that extends from Building:

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -20,3 +20,24 @@ export default class SkyHighBuilding extends Building {
     return `Evacuate slowly the ${this.floors} floors.`;
   }
 }
+/* eslint-disable no-underscore-dangle */
+import Building from "./5-building";
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  // set floors(f) {
+  //   this._floors = f;
+  // }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors.`;
+  }
+}
