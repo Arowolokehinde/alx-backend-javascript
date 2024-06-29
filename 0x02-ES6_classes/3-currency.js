2f55c139dd26aa04f105061d79edf9dad6bb12b2
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-06-29 17:23:08 +0100

    created a class currency and gave it attributes of code and name

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,23 +1,49 @@
-/* eslint-disable no-underscore-dangle */
+// /* eslint-disable no-underscore-dangle */
+// export default class Currency {
+//   constructor(code, name) {
+//     (this._code = code), (this._name = name);
+//   }
+
+//   get code() {
+//     return this._code;
+//   }
+
+//   set code(code) {
+//     this._code = code;
+//   }
+
+//   get name() {
+//     return this._name;
+//   }
+
+//   set name(name) {
+//     this._name = name;
+//   }
+
+//   displayFullCurrency() {
+//     return `${this.name} (${this.code})`;
+//   }
+// }
 export default class Currency {
   constructor(code, name) {
-    (this._code = code), (this._name = name);
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(code) {
     this._code = code;
+    this._name = name;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(name) {
-    this._name = name;
+  set name(n) {
+    this._name = n;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(c) {
+    this._name = c;
   }
 
   displayFullCurrency() {
