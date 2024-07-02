f2dffe9d6f9faf2762794913d49b33ddb6d8c12b
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-07-02 17:29:19 +0100

    created a function that checks if an array exists in a set

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,9 +1,20 @@
 /* eslint-disable quotes */
-export default function hasValuesFromArray(sets, arr) {
-  for (const elemet of arr) {
-    if (!sets.has(elemet)) {
-      return false;
+// export default function hasValuesFromArray(sets, arr) {
+//   for (const elemet of arr) {
+//     if (!sets.has(elemet)) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+/* eslint-disable array-callback-return */
+export default function hasValuesFromArray(set, list) {
+  let elemt = true;
+  list.map((x) => {
+    if (!set.has(x)) {
+      elemt = false;
     }
-  }
-  return true;
+  });
+
+  return elemt;
 }
