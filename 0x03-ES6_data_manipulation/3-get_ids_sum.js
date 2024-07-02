b224f40ee943390905b33f51fd279c165fd9d3c3
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-07-02 16:50:35 +0100

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
-export default function getStudentIdsSum(list) {
-  return list.reduce(
+export default function getStudentIdsSum(arr) {
+  return arr.reduce(
     (accumulator, currentValue) => accumulator + currentValue.id,
-    0
+    0,
   );
 }
