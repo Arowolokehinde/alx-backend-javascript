9538d3a7f89c48db764a25ce6a12e0ce33605902
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-07-02 15:41:40 +0100

    creted a function that returns the sum of all the students ids

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,9 +1,3 @@
-// export default function getStudentIdsSum(arr) {
-//   return arr.reduce(
-//     (accumulator, currentValue) => accumulator + currentValue.id,
-//     0
-//   );
-// }
 export default function getStudentIdsSum(list) {
   return list.reduce(
     (accumulator, currentValue) => accumulator + currentValue.id,
