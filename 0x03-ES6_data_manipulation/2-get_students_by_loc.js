b99b52fc70ccfe4ece4f0ab27b5160be17da5c99
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-07-02 15:34:24 +0100

    creted a function that returns the sum of all the students ids

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,6 @@
-/* eslint-disable no-underscore-dangle */
-
-export default function getStudentsByLocation(arr, city) {
-  return arr.filter((obj) => obj.location === city);
+// export default function getStudentsByLocation(arr, city) {
+//   return arr.filter((obj) => obj.location === city);
+// }
+export default function getStudentsByLocation(list, city) {
+  return list.filter((obj) => obj.location === city);
 }
