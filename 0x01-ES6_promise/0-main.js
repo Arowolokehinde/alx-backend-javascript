9ed1f3ec504989023baad58b47a996782222d9c1
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-07-26 14:01:17 +0100

    0-promise.js

---
+++ 0x01-ES6_promise/0-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable quotes */
+import getResponseFromAPI from "./0-promise";
+
+const response = getResponseFromAPI();
+console.log(response instanceof Promise);
---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+/* eslint-disable quotes */
+
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    resolve("Success");
+  });
+}
