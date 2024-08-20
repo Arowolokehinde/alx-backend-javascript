de6811daf1edd5c9b7e6a41bd3339689d948d42c
Arowolokehinde <arowolokehinde321@gmail.com>
2024-08-20 11:52:44 +0100

    created a function that displayed a message to the standard output

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,11 @@
 /* Displays a message on to the STDOUT*/
 
-function displayMessage(content) {
-    console.log(content);
-}
+// function displayMessage(content) {
+//     console.log(content);
+// }
+
+const displayMessage = (msg) => {
+    console.log(msg);
+};
 
 module.exports = displayMessage;
